090df0b6f8fb63c4fc64a3a51f855abbd1387e71
unknown <gadya3009@gmail.com>
2024-01-22 15:38:07 +0600

    commit message

--- zadanie.py
+++ zadanie.py
@@ -1,13 +1,26 @@
 kol = 0
-diaposon = int(input())
 with open(r'Perepis.txt', 'r') as f:
     
-    
+    print('punkt_a')
     for i in f:
-        
         s = i.split() 
-        print(s[3][6:10])
-        if int(s[3][6:10]) < diaposon:
-            print(s[0],s[1],s[2])
+        if int(s[3][6:10]) < 1978:
+            print(s[0])
             kol+=1
-print(kol)
+            
+    print(kol)
+with open(r'Perepis.txt', 'r') as f:
+    print('punkt_b')
+    fist_chislo = int(input())
+    second_chislo = int(input())
+
+    for i in f:
+        s = i.split() 
+        if (fist_chislo < int(s[3][6:10]) and int(s[3][6:10]) < second_chislo):
+            print(s[0],s[1],s[2],s[3][6:10])
+
+
+
+
+
+
